97a2172a0dd2cac4e6044c2e9a80939aa0cc2fc7
Shihab <shihabmuhtasim@gmail.com>
2023-08-11 16:16:59 +0600

    Updated order view

--- resources/views/vendor/v_orders.blade.php
+++ resources/views/vendor/v_orders.blade.php
@@ -42,15 +42,19 @@
     <tr class="text-center">
 
 
-        <th>ID</th>
-        <th>Name</th>
+        <th>Order ID</th>
+        <th>Customer Name</th>
+
+        <th>phone</th>
+        
+        <th>address</th>
+        <th>Product Name</th>
         <th>Image</th>
         <th>Price</th>
-        <th>Discount Price</th>
         <th>Quantity</th>
-        <th>Day count on price</th>
+        <th>Start date</th>
+        <th>End date</th>
         <th>Vendor</th>
-        <th>Edit</th>
         <th>Delete</th>
     </tr>
 
@@ -60,6 +64,9 @@
         <tr class='text-center'>
         
             <td>{{$product_data->id}}</td>
+            <td>{{$product_data->name}}</td>
+            <td>{{$product_data->phone}}</td>
+            <td>{{$product_data->address}}</td>
             <td>{{$product_data->product_title}}</td>
             <td>
             <img src="/added_products/{{$product_data->image}}" class="product-image">
@@ -68,15 +75,21 @@
 
             </td>
             <td>{{$product_data->price}}</td>
-            <td>{{$product_data->discounted_price}}</td>
             <td>{{$product_data->quantity}}</td>
-            <td>{{$product_data->days}}</td>
+            <td>{{ $product_data->created_at->format('Y-m-d') }}</td>
+@php
+    $daysToAdd = $product_data->day; // Change this to the number of days you want to add
+    $newDate = $product_data->created_at->addDays($daysToAdd);
+@endphp
+
+<td>{{ $newDate->format('Y-m-d') }}</td>
+
+
             <td>{{$product_data->vendor_name}}</td>
 
-
 	            
-            <td><a onclick="return confirm('Confirm Edit?')" class="btn btn-success" href="{{url('v_edit_product',$product_data->id)}}">Edit</a></td>
-            <td><a onclick="return confirm('Confirm Delete?')" class="btn btn-danger" href="{{url('v_delete_product',$product_data->id)}}">Delete</a></td>
+
+            <td><a onclick="return confirm('Confirm Delete?')" class="btn btn-danger" href="{{url('delete_v_orders',$product_data->id)}}">Delete</a></td>
         </tr>
 
 @endforeach
